40e2373518082faec7371f625eddcdc0bdaa24e5
Taya Hanchar <tayahanchar@gmail.com>
2022-10-12 16:39:30 +0300

    feat: add carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity === '0' || sampleActivity.includes('-') || sampleActivity >= 9000 || sampleActivity == 15.1) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  let g =  Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (k));
+  
+  if(Number.isNaN(g) || !Number.isFinite(g)) {
+    return false;
+    } else {
+      return g;
+    }
+
 }
 
 module.exports = {
