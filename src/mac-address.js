f2067a1f0e789cd69118d05e70f2d47d11e315a6
Taya Hanchar <tayahanchar@gmail.com>
2022-10-13 15:10:00 +0300

    feat: add mac-address and get-email tasks

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let num = n.split('-').join('');
+  if (parseInt(num, 16)) {
+    return true
+  } else {
+    return false
+  }
 }
+
 module.exports = {
   isMAC48Address
 };
