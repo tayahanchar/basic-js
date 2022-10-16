26165db0d61a0d8135b1c889869cb51e83f5a38b
Taya Hanchar <tayahanchar@gmail.com>
2022-10-16 14:41:31 +0300

    feat: add extended-repeat task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let {repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator ='|'} = options;
+
+  let repeatStr = [];
+  for (let i = 0; i < repeatTimes; i++) {
+    repeatStr.push(str);
+  }
+
+  let repeataddition = [];
+  for (let i = 0; i < repeatTimes; i++) {
+    repeataddition.push([]);
+    for (let k = 0; k < additionRepeatTimes; k++) {
+      if(addition === null) {
+        repeataddition[i].push(`${addition}`);
+      } else {
+        repeataddition[i].push(addition);
+      }
+    }
+  }
+
+  let repeatadditionItem = []
+
+  repeataddition.map(element => {
+    repeatadditionItem .push(element.join(`${additionSeparator}`))
+  });
+
+  for (let i = 0; i < repeatTimes; i++) {
+    repeatStr[i] = repeatStr[i] + repeatadditionItem[i];
+  }
+
+  return repeatStr.join(`${separator}`);
 }
 
 module.exports = {
